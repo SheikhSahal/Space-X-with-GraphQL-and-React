c253f92f7566ece017c0e819b995267f289e3213
Muhammad Sahal Qasim <36259752+SheikhSahal@users.noreply.github.com>
2020-12-29 18:32:48 +0500

    add bootstarp in SpaceX App

--- src/components/Mission/MissionList.tsx
+++ src/components/Mission/MissionList.tsx
@@ -8,13 +8,23 @@ interface Props {
 const MissionList: React.FC<Props> = ({ data }) => {
     return (
         <div>
-            <ul>
+
+            <div className="col-md-4">
+
+                <div className="card">
+                    <img className="card-img-top" src="..." alt="Card image cap" />
+                    <div className="card-body">
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            {/* <ul>
                 {data.launches?.map((launchobj, ind) => {
                     return (<li key={ind}>
                         {launchobj?.mission_name}
                     </li>)
                 })}
-            </ul>
+            </ul> */}
 
 
         </div>
